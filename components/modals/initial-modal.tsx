9e961768216ec9febaa711a926b27e8c65029bd9
aaditya9899 <aadityaprasad9899@gmail.com>
2023-12-18 13:14:54 +0530

    upload thing implemented

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,7 +24,7 @@ FormMessage
 } from  "@/components/ui/form"
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
+import { FileUpload } from "../file-upload";
 const formSchema=z.object({
     name: z.string().min(1,{
         message:"Server name is required."
@@ -71,7 +71,22 @@ if(!isMounted){
        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <div className="space-y-8 px-6">
             <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+               <FormField
+                control={form.control}
+                name="imageUrl"
+                render={({field})=>(
+                    <FormItem>
+                        <FormControl>
+                            <FileUpload
+                            endpoint="serverImage"
+                            value={field.value}
+                            onChange={field.onChange}
+                            
+                            />
+                        </FormControl>
+                    </FormItem>
+                )}
+               />
             </div>
         <FormField
             control={form.control}
